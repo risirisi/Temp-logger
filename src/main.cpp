c72360efad60a9d0876ef1d3a80d45ae94a1407d
risirisi <risisara@gmail.com>
2026-02-15 19:52:55 +1100

    testing build and upload, serial

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Hello");
+  delay(1000);
+
 }
 
 // put function definitions here:
