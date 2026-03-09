6868daff2668a92473a7f08a768afeb81e3daed0
risirisi <risisara@gmail.com>
2026-03-09 20:20:07 +1100

    logger values

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <Arduino.h>
 
-unsigned long last_time = 0;
+unsigned long last_time = 0;  // for timer values you always use unsigned long
 float temp = 25.0;
 
-
-
 void setup() {
+
 Serial.begin(115200);
-
-delay(2000);
-
 Serial.println("time_ms, temp_c");
+
 }
 
 void loop() {
 
   if ( millis() - last_time >= 1000 ) {
+
+    // so the timer is basically every second
 
     last_time = millis();
     Serial.print(last_time);
